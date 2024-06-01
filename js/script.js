5f07833dde4630dab847937f6e11571fc827fa21
IsraMalik <154972136+IsraMalik@users.noreply.github.com>
2024-06-01 19:32:44 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing", {
+  strings: [" ", "Human", "Ojek Online", "Weird Human", "Sesuai Gosip Tetangga"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
